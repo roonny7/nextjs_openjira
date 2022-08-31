ce39aef0537acfaca22ec5a88751a64094bb8d80
roonny <roonny7@hotmail.com>
2022-08-31 16:12:59 -0500

    miercoles 31

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -35,8 +35,18 @@ export const EntriesProvider:FC <Props>= ({ children}) => {
           dispatch( { type: '[Entry] - Add-Entry', payload : data})
      }
 
-     const updateEntry = (entry: Entry) => {
-          dispatch({ type: '[Entry] - Entry-Updated', payload: entry })
+     const updateEntry = async( { _id, description, status}:Entry) => {
+          try {
+              // alert("aqui");
+              const {data} = await entriesApi.put<Entry>(`/entries/${_id}`, { description : description, status : status});
+              dispatch({ type: '[Entry] - Entry-Updated', payload: data })
+          }
+          catch (error) {
+               console.log(error);
+          }
+          
+          
+          
 
      }
 
--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { db } from '../../../database';
 import { Entry, IEntry } from '../../../models/';
+import { disconnect } from '../../../database/db';
 
 type Data = 
 |   { message: string }
@@ -20,7 +21,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     switch ( req.method){
         case 'PUT':
             return updateEntry( req, res);
-
+        case 'GET':
+            return getEntry( req, res);
         default: 
             return res.status(400).json({ message : ' Método no existe'})
 
@@ -45,9 +47,43 @@ const updateEntry = async ( req: NextApiRequest, res : NextApiResponse<Data>) =>
 
     const { description = entryToUpdate.description, status = entryToUpdate.status}  = req.body;
 
-    const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status}, { runValidators : true, new : true})
+    try {
+        const updatedEntry = await Entry.findByIdAndUpdate(id, { description, status}, { runValidators : true, new : true})
+        await db.disconnect();
+        res.status(200).json( updatedEntry!);
 
-    res.status(200).json( updatedEntry!);
+    } catch (error:any) {
+        await db.disconnect();
+        res.status(400).json({ message : error.errors.status.message})
+    }
+    
+    
+    //es lo mismo : 
+    /*entryToUpdate.description = description;
+    entryToUpdate.status = status;
+    await entryToUpdate.save();*/
 
+    
+
+    
+}
+
+
+const getEntry = async ( req: NextApiRequest, res : NextApiResponse<Data>) => {
+    
+    const { id } = req.query;
+    
+    await db.connect();
+
+    const entryInDB = await Entry.findById(id);
     await db.disconnect();
+
+    if (!entryInDB){
+        await db.disconnect();
+        return res.status(400).json({message : 'No hay entradas con ese id'+id});
+    }
+
+    res.status(200).json( entryInDB);
+
+    
 }
